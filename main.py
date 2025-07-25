ea415b0fd54fc257433cfe225c875a3f12699073
Magomed-Ameen <muhammadameenk@yahoo.com>
2025-07-25 21:43:55 +0500

    Using environment variables for firebase config

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Load the Firebase credentials from the secret
-firebase_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
-cred_dict = json.loads(firebase_json)
-cred = credentials.Certificate(cred_dict)
+firebase_config = {
+    "type": os.environ["FIREBASE_TYPE"],
+    "project_id": os.environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ["FIREBASE_CLIENT_ID"],
+    "auth_uri": os.environ["FIREBASE_AUTH_URI"],
+    "token_uri": os.environ["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_CERT_URL"],
+    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_CERT_URL"]
+}
+
+cred = credentials.Certificate(firebase_config)
 
 firebase_admin.initialize_app(cred, {
     "databaseURL": os.getenv("FIREBASE_DATABASE_URL")
